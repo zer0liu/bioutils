d7ffcb8de9175d193dc71eb9e9a6bf54bd413a5d
zer0liu <26947223+zer0liu@users.noreply.github.com>
2019-04-17 17:18:27 +0800

    Working.

--- 10x_genomics/fetch_10xreads.pl
+++ 10x_genomics/fetch_10xreads.pl
@@ -15,7 +15,7 @@
 
 =head1 VERSION
 
-    0.0.1   - 2019-04-04
+    0.0.1   - 2019-04-17
 
 =cut
 
@@ -23,6 +23,78 @@ use 5.12.1;
 use strict;
 use warnings;
 
+use boolean;
+use Getopt::Long;
+use MongoDB;
+use Smart::Comments;
+
+#===========================================================
+#
+#                   Predefined Variables
+#
+#===========================================================
+
+# The amount of time in milliseconds to wait for a new connection to 
+# a server.
+# Default: 10,000 ms
+my $connect_timeout_ms  = 10_000;   # i.e., 10 s
+
+# the amount of time in milliseconds to wait for a reply from the 
+# server before issuing a network exception.
+# Default: 30,000 ms
+my $socket_timeout_ms   = 1_800_000;  # i.e., 120 s
+
+#===========================================================
+#
+#                   Main Program
+#
+#===========================================================
+
+my $host    = '127.0.0.1';
+my $port    = '27017';
+my $level   = 'umi';
+
+my ($db, $user, $pwd);
+
+GetOptions(
+    "d=s"       => \$db,
+    "host=s"    => \$host,
+    "port=s"    => \$port,
+    "user=s"    => \$user,
+    "pwd=s"     => \$pwd,
+    "h"         => sub { die usage() },
+);
+
+unless ($db) {
+    warn "[ERROR] Database name is required!\n";
+    die usage();
+}
+
+unless ($level eq 'cb' || $level eq 'umi') {
+    warn "[ERROR] Only level 'cb' or 'umi' supported!\n";
+    die usage();
+}
+
+# Generate connection string URI
+my $conn_uri= 'mongodb://';
+
+# Append user & pwd
+$conn_uri   = $conn_uri . $user . ':' . $pwd . '@'
+    if ($user && $pwd);
+
+# Append host & ip
+$conn_uri   = $conn_uri . $host . ':' . $port . '/';
+
+# Append connection options
+$conn_uri   = $conn_uri . '?' . 
+    'connectTimeoutMS=' . $connect_timeout_ms .
+    '&' . 'socketTimeoutMS=' . $socket_timeout_ms;
+
+# Connect to database
+my $mongo_client    = MongoDB->connect($conn_uri);
+
+# Get given database
+my $mongo_db  = $mongo_client->get_database( $db );
 
 
 
