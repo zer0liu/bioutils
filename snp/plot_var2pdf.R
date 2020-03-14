732dc626292811c6e3192f8a592a68a53b84968c
zer0liu <26947223+zer0liu@users.noreply.github.com>
2020-03-15 00:17:18 +0800

    More arguments and fine plot

--- snp/plot_var2pdf.R
+++ snp/plot_var2pdf.R
@@ -16,31 +16,99 @@
 #   0.0.1   2018-04-30
 #   0.1.0   2018-07-20
 #   0.1.1   2018-07-25  More details for legend
-#
+#   0.2.0   2020-03-14  More options for fine tuning of plot
 
 library(ggplot2)
+suppressPackageStartupMessages( library(R.utils) )
+
+# Usage information
+usage <- "
+Plot SNP information according to the output of script 'stat_var_codon.pl'.
+Usage:
+  Rscript plot_var2pdf.R --in=<fin> [--out=<fout>] [--strname] [--grid]
+Arguments:
+  --in=<fin>    Input file, created by the script 'stat_var_codon.pl'
+  --out=<fout>  Output file. Optional. 
+                Default 'vsite.pdf'.
+Options:
+  --strname     Show strain/sequence name.
+                Default do not show.
+  --grid        Show gray grid.
+                Default do not show.
+"
 
 # Parse command arguments
-args <- commandArgs( trailingOnly = TRUE)
+args <- commandArgs( trailingOnly = TRUE, asValues = TRUE )
+#args <- commandArgs( trailingOnly = TRUE)
 
-if (length(args)==0) {
-    stop("[ERROR] At least one argument must be supplied (input file)!\n", call.=FALSE)
-} else if (length(args)==1) {
-    fin     <- args[1]
-    # default output file
-    print("[NOTE] Default output file 'vsite.pdf'.\n")
-    fout    <- "vsite.pdf"
+#if (length(args)==0) {
+#    stop(paste("[ERROR] At least one argument must be supplied (input file)!\n", usage), 
+#        call.=FALSE)
+#} else if (length(args)==1) {
+#    fin     <- args[1]
+#    # default output file
+#    print("[NOTE] Default output file 'vsite.pdf'.\n")
+#    fout    <- "vsite.pdf"
+#} else {
+#    fin     <- args[1]
+#    fout    <- args[2]
+#}
+
+if ( length(args$`in` ) == 0 ) {
+    stop(paste("[ERROR] At least one argument must be supplied (input file)!\n", usage), 
+        call.=FALSE)
 } else {
-    fin     <- args[1]
-    fout    <- args[2]
+    fin = args$`in`
+}
+
+if ( length(args$out ) == 0 ) {
+    # default output file
+    print("[NOTE] Default output file 'vsites.pdf'.\n")
+    fout    <- "vsites.pdf"
+} else {
+    fout    <- args$out
 }
 
 # Read input file
 vsite <- read.table(fin, header = TRUE)
 
-# p <- ggplot(vsite, aes(x=Location, y=Strain, fill=Value)) + geom_tile() + theme_classic() + scale_fill_manual(values = c("grey30", "white", "#4daf4a", "#e41a1c", "#377eb8", "#ff7f00")) + theme(axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank(), axis.line.y = element_blank())
+# Operation on viste$Strain, for headmap plot in given order
+vsite$Strain <- as.character( vsite$Strain )
+vsite$Strain <- factor(vsite$Strain, levels = unique(vsite$Strain) )
 
-p <- ggplot(vsite, aes(x=Location, y=Strain, fill=Value)) + geom_tile() + theme_classic() +  theme(axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank(), axis.line.y = element_blank() ) + scale_fill_manual(values = c("grey30", "white", "#4daf4a", "#e41a1c", "#377eb8", "#ff7f00"), breaks=c("-", "a", "i", "n", "s", "u"), labels=c("Gap", "Not changed", "Iner-gene", "Nonsynonymous", "Synonymous", "UTR"), name=NULL)
+# Plotting
+p <- ggplot(vsite, aes(x=Location, y=Strain, fill=Value))  
+
+# Whether show/hide gray grid
+if ( args$grid == TRUE ) {  # Show 
+    p <- p + geom_tile( color = "gray" )
+} else {    # Hide
+    p <- p + geom_tile()
+}
+
+# Hide background grid
+p <- p + theme_classic()
+
+# Whether show/hide strain/sequence name
+if ( args$strname == TRUE ) {   # Show 
+    p <- p + theme(
+        axis.title.y = element_blank(), 
+        # axis.text.y = element_blank(), 
+        axis.ticks.y = element_blank(), 
+        axis.line.y = element_blank() ) +
+        ylim( rev( levels( vsite$Strain ) ) )   # Plot heatmap y label in line order
+} else {    # Hide
+    p <- p + theme(
+        axis.title.y = element_blank(), 
+        axis.text.y = element_blank(), 
+        axis.ticks.y = element_blank(), 
+        axis.line.y = element_blank() ) 
+}
+
+p <- p + scale_fill_manual(
+        values = c("grey30", "white", "#4daf4a", "#e41a1c", "#377eb8", "#ff7f00"), 
+        breaks=c("-", "a", "i", "n", "s", "u"), 
+        labels=c("Gap", "Not changed", "Intergenic Region", "Nonsynonymous", "Synonymous", "UTR"), name=NULL)
 
 ggsave(fout, p, device = "pdf")
 
