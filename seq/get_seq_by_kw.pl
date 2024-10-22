57137d7881acd70e875d13446fcd4de103c00ebd
zeroliu <zeroliu@gnode02>
2024-10-22 12:21:55 +0800

    seq/get_seq_by_kw.pl - New feature to parse sequence description.wq

--- seq/get_seq_by_kw.pl
+++ seq/get_seq_by_kw.pl
@@ -16,6 +16,7 @@
 =head1 VERSION
 
     0.0.1   2015-02-04
+    0.0.2   2024-10-22  Add feature to parse sequence description.
 
 =cut
 
@@ -24,20 +25,44 @@ use strict;
 use warnings;
 
 use Bio::SeqIO;
+use Getopt::Long;
 #use Smart::Comments;
 
 my $usage = << "EOS";
 Get sequences from a multi-FASTA file accroding to a keywords file, then
 output to STDOUT.
 Usage:
-  get_seq_by_kw.pl <seq> <kw>
+  get_seq_by_kw.pl -i <seq> -k <kw> [-a]
 Arguments:
-  <seq> FASTA format sequence file.
-  <kw>  Keywords file. One keywords in each line.
+  -i <seq>: FASTA format sequence file.
+  -k <kw>:  Keywords file. One keywords in each line.
+  -a:       Optional. Default check sequence ID only. Use this option
+            will check sequence description either.
+Note:
+- Output to STDOUT.
+Attention:
+- Please use Escape ('\\') if these characters in the keyword string:
+  [ => \\[, ] => \\], - => \\-, . => \\., etc.
+  e.g., for searching "[feat=CDS]", use "\\[feat=CDS\\]" in the keyword
+  file.
 EOS
 
-my $fseq    = shift or die $usage;
-my $fkw     = shift or die $usage;
+# my $fseq    = shift or die $usage;
+# my $fkw     = shift or die $usage;
+
+my ($fseq, $fkw, $F_a);
+
+GetOptions(
+    "i=s"   => \$fseq,
+    "k=s"   => \$fkw,
+    "a"     => \$F_a,
+);
+
+die "[ERROR] Need input sequence file!\n$usage"
+    unless ($fseq);
+
+die "[ERROR] Need input keyword file!\n$usage"
+    unless ($fkw);
 
 # Parse keywords file
 my @keywords;
@@ -71,11 +96,22 @@ my $o_seqo  = Bio::SeqIO->new(
 );
 
 while (my $o_seq = $o_seqi->next_seq) {
-    my $id  = $o_seq->id;
+    my $id      = $o_seq->id;
+    my $desc    = $o_seq->desc;
 
-    for my $kw ( @keywords ) {
-        $o_seqo->write_seq( $o_seq ) if ( $id =~ /$kw/ );
+    unless ($F_a) {
+	    for my $kw ( @keywords ) {
+	        $o_seqo->write_seq( $o_seq ) if ( $id =~ /$kw/ );
+	    }
+    }
+    else {
+        for my $kw ( @keywords ) {
+            $o_seqo->write_seq( $o_seq ) if ( $desc =~ /$kw/ );
+        }
     }
 }
 
 exit 0;
+
+# DONE
+
